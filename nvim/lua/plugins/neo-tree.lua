171227eb2741ed51a364fa85f3cd9b9fb5eab31a
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2023-05-01 13:37:03 -0400

    added neotree fullscreen

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      width = "100%",
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+  },
+}
